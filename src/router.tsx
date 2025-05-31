ec2ff32cf422718d2bcef3a1987817b042b90961
Darío Guillén <guillendario@gmail.com>
2025-05-30 18:37:12 -0600

    feat: app layout added

--- src/router.tsx
+++ src/router.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LoginView from "./views/LoginView";
 import RegisterView from "./views/RegisterView";
 import AuthLayout from "./layouts/AuthLayout";
+import AppLayout from "./layouts/AppLayout";
 
 const Router = () => {
   return (
@@ -10,6 +11,9 @@ const Router = () => {
         <Route element={<AuthLayout />}>
           <Route path="/auth/login" element={<LoginView />} />
           <Route path="/auth/register" element={<RegisterView />} />
+        </Route>
+        <Route path="/admin" element={<AppLayout />}>
+          <Route index={true} />
         </Route>
       </Routes>
     </BrowserRouter>
