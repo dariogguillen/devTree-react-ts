c2c2fc108a42222341f07c985fcdd47738187e27
Darío Guillén <guillendario@gmail.com>
2025-06-02 14:45:34 -0600

    refactor: using interceptor to validate token and redirect to login

--- src/api/DevTreeAPI.ts
+++ src/api/DevTreeAPI.ts
@@ -1,14 +1,12 @@
 import { isAxiosError } from "axios";
 import api from "../config/axios";
+import type { User } from "../types";
 
 export const getUser = async () => {
   try {
-    const { data } = await api("/user", {
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem("AUTH_TOKEN")}`,
-      },
-    });
-    return data;
+    const { data } = await api<{ response: string; user: User }>("/user");
+    console.log({ getUser: data });
+    return data.user;
   } catch (error) {
     if (isAxiosError(error) && error.response) {
       throw new Error(error.response.data.error);
